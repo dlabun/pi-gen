a43081b1a0572189fa8cdf3a7f968ab842dec341
Douglas Labun <dlabun@labunnuclear.com>
2018-08-12 17:00:52 -0400

    Update touch beep support

--- stage5/00-touch-beep/00-run.sh
+++ stage5/00-touch-beep/00-run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
-install -m 644 files/touch-beep.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -m 644 files/touch-beep "${ROOTFS_DIR}/opt/ComfileTech/touch/Beep"
+install -m 755 files/touch-beep.service "${ROOTFS_DIR}/etc/systemd/system/"
+install -m 755 files/pigpio.service "${ROOTFS_DIR}/etc/systemd/system/"
+mkdir -p /opt/ComfileTech/touch/Beep
+install -m 755 files/touch-beep "${ROOTFS_DIR}/opt/ComfileTech/touch/Beep/"
