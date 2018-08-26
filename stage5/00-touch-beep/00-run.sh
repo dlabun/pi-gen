0386b87c8641776a9d044d0692fe31cc2ee73401
Douglas Labun <18444350+dlabun@users.noreply.github.com>
2018-08-26 16:04:49 -0400

    Touch Beep now functioning, Disabled building NOOBS on last stage to speed up process.

--- stage5/00-touch-beep/00-run.sh
+++ stage5/00-touch-beep/00-run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
 install -m 755 files/touch-beep.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -m 755 files/pigpio.service "${ROOTFS_DIR}/etc/systemd/system/"
-mkdir -p /opt/ComfileTech/touch/Beep
+install -m 755 files/pigpiod.service "${ROOTFS_DIR}/etc/systemd/system/"
+mkdir -p ${ROOTFS_DIR}/opt/ComfileTech/touch/Beep
 install -m 755 files/touch-beep "${ROOTFS_DIR}/opt/ComfileTech/touch/Beep/"
+on_chroot << EOF
+systemctl enable pigpiod
+systemctl enable touch-beep
+EOF
